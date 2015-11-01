a645fb968d4e4c78183f277dd462b444df07db8f
Davide Valsecchi <valsecchi.davide94@gmail.com>
2015-11-02 00:37:43 +0100

    Added script to start bot

---
+++ execute-bot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export W2L_INSTANCE_NAME="w2l-dev"
+
+docker run  -e PYTHONPATH="/pywikibot" -ti --name pywikibot --hostname pywikibot \
+ --link ${W2L_INSTANCE_NAME}-websrv:www.wikitolearn.org \
+ --link ${W2L_INSTANCE_NAME}-websrv:pool.wikitolearn.org \
+ --link ${W2L_INSTANCE_NAME}-websrv:meta.wikitolearn.org \
+ --link ${W2L_INSTANCE_NAME}-websrv:it.wikitolearn.org \
+ --link ${W2L_INSTANCE_NAME}-websrv:en.wikitolearn.org \
+ --link ${W2L_INSTANCE_NAME}-websrv:de.wikitolearn.org \
+ --link ${W2L_INSTANCE_NAME}-websrv:es.wikitolearn.org \
+ --link ${W2L_INSTANCE_NAME}-websrv:fr.wikitolearn.org \
+ --link ${W2L_INSTANCE_NAME}-websrv:pt.wikitolearn.org \
+ --link ${W2L_INSTANCE_NAME}-websrv:sv.wikitolearn.org \
+wikitolearn/pywikibot
+
